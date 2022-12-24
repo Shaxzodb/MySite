47a2ab6ba47e9d1b152661749eb68b9e6fc5f86b
shaxzodb <shaxzodbmaster@gmail.com>
2022-12-24 19:30:56 +0500

    My site Add

--- build.sh
+++
@@ -1,6 +0,0 @@
-echo "Collecting staticfiles"
-python manage.py collectstatic --noinput
-echo "Running makemigrations"
-python manage.py makemigrations
-echo "Running database migrations"
-python manage.py migrate --noinput
