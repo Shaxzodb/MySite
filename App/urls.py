a6088f8bd6870de91ceba1ad891e8cd566ef1e45
shaxzodb <shaxzodbmaster@gmail.com>
2022-12-24 19:53:29 +0500

    my django site add

--- App/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import Base
-
-urlpatterns = [
-    path('',Base,name='base')
-]
--- Articles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ArticleListView, ArticleDetailView
-
-urlpatterns = [
-    path('list/', ArticleListView.as_view() ,name = 'list'),
-    path('news/<slug:slug>', ArticleDetailView.as_view(),name = 'detail')
-]
