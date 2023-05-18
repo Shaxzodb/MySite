9ec9e9c88794489b892984f4bcdea6eb679ab574
Shaxzod Bahtiyarov <shaxzodbmaster@gmail.com>
2023-05-19 02:44:55 +0500

    Update views.py
    add Forms instance

--- Articles/views.py
+++ Articles/views.py
@@ -119,3 +119,22 @@ def article_delete(request, slug):
         if request.user.id == article.author.id:
             article.delete()
     return redirect('profile', request.user.profile.slug)
+
+
+''' from django.forms import ModelForm
+>>> from myapp.models import Article
+
+# Create the form class.
+>>> class ArticleForm(ModelForm):
+...     class Meta:
+...         model = Article
+...         fields = ["pub_date", "headline", "content", "reporter"]
+...
+
+# Creating a form to add an article.
+>>> form = ArticleForm()
+
+# Creating a form to change an existing article.
+>>> article = Article.objects.get(pk=1)
+>>> form = ArticleForm(instance=article)
+'''
